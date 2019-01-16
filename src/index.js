aa8963332e021a3e5cc5476e3c82b8afe7939a55
8u4a <iordanalecsandru@yahoo.com>
2019-01-16 21:52:32 +0200

    Loading screen, seasons which change when overiding in dev-tools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 
 /* const App = () => {
@@ -19,27 +20,32 @@ class App extends React.Component {
 
         componentDidMount() {
             window.navigator.geolocation.getCurrentPosition(
-                position => this.setState({lat: position.coords.latitude}),
-                    
+                position => this.setState({lat: position.coords.latitude}),                    
                 err => this.setState({errorMessage: err.message})                  
             );            
-        };
-       //always do data loading inside the compomonentDidMount not  in the constructor
-       // lifecycling methods componentDidMount/componentDidUpdate/componentWillUnmount
+        }
 
-    // React says we have to define render!!!!!!
-                render() {            
+        renderContent(){
             if (this.state.errorMessage && !this.state.lat) {
-                return <div>Error: {this.state.errorMessage}</div>
+                return <div>Error: {this.state.errorMessage}</div>;
             }
 
             if (!this.state.errorMessage && this.state.lat) {
-                return <SeasonDisplay lat={this.state.lat} />
+                return <SeasonDisplay lat={this.state.lat} />;
             }
 
-            return <div>Loading!</div>
-        };
-    };
+            return <Spinner message="Please accept location request" />;
+        
+        }
+       //always do data loading inside the compomonentDidMount not  in the constructor
+       // lifecycling methods componentDidMount/componentDidUpdate/componentWillUnmount
+
+        render() {
+            return <div className="border red">{this.renderContent()}</div>
+            
+            
+        }
+    }
 
 
 ReactDOM.render(
